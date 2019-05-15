7c83f587ee925d29456d300b21336742334b3f6a
ParrishJ <jarparrish@gmail.com>
2019-05-15 13:38:56 -0700

    Information added to actions and reducers

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,6 +4,21 @@ export const FETCH_CHARS_START = "FETCH_CHARS_START";
 export const FETCH_CHARS_SUCCESS = "FETCH_CHARS_SUCCESS";
 export const FETCH_CHARS_FAILURE = "FETCH_CHARS_FAILURE";
 
+export const getChars = () => dispatch => {
+  dispatch({ type: FETCH_CHARS_START });
+  axios
+    .get("https://swapi.co/api/people/")
+    .then(res => {
+      dispatch({ type: FETCH_CHARS_SUCCESS, payload: res.data });
+    })
+    .catch(err => {
+      dispatch({
+        type: FETCH_CHARS_FAILURE,
+        payload: "Error Message"
+      });
+    });
+};
+
 // we'll need to create 3 different action types here.
 // one for fetching, one for success and one for failure
 
--- src/reducers/starWarsReducer.js
+++ src/reducers/starWarsReducer.js
@@ -1,10 +1,36 @@
-import /* we need our action types here*/ "../actions";
+import {
+  FETCH_CHARS_START,
+  FETCH_CHARS_SUCCESS,
+  FETCH_CHARS_FAILURE
+} "../actions";
+
 const initialState = {
-  characters: []
+  characters: [],
+  isFetching: false,
+  error: ''
   // Array characters, Boolean fetching, null error.
 };
 export const charsReducer = (state = initialState, action) => {
   switch (action.type) {
+    case FETCH_CHARS_START:
+      return {
+        ...state,
+        isFetching: true,
+        error: ""
+      };
+    case FETCH_CHARS_SUCCESS:
+      return {
+        ...state,
+        isFetching: false,
+        error: "",
+        characters: action.payload
+      }
+    case FETCH_CHARS_FAILURE:
+      return {
+        ...state,
+        isLoading: false,
+        error: action.payload
+      };
     // Fill me in with the important reducers
     // action types should be FETCHING, SUCCESS and FAILURE
     // your switch statement should handle all of these cases.
@@ -12,3 +38,5 @@ export const charsReducer = (state = initialState, action) => {
       return state;
   }
 };
+
+export default charsReducers
